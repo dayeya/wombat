26de7d368e4d9a7ca5f2eb87c4507e54d70f0ba7
Daniel <clipchamp.daniel@gmail.com>
2025-03-16 17:27:55 +0200

    gh: common.hpp -> common utils, wtypes.hpp -> frequently used types within the compiler

---
+++ src/wombat_common/wtypes.hpp
@@ -0,0 +1,27 @@
+#ifndef TYPES_HPP_
+#define TYPES_HPP_
+
+#include <functional>
+#include <memory>
+#include <optional>
+#include <expected>
+
+template<typename Ret, typename ...Args>
+using Closure = std::function<Ret(Args...)>;
+
+template<typename T>
+using Ptr = std::unique_ptr<T>;
+
+template<typename T> 
+Ptr<T> mk_ptr(T&& v) {
+    return std::make_unique<T>(std::forward<T>(v));
+}
+
+template<typename T>
+using Option = std::optional<T>;
+
+template<typename T, typename E>
+using Result = std::expected<T, E>;
+
+
+#endif // TYPES_HPP_
